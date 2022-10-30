56cc90030c5923306d600246fc630fde735d62f8
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-10-30 13:17:08 -0400

    calling youtube API with query and key.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(function () {
     //focus event handler
     $(searchField).on('focus', function () {
         $(this).animate({
-            width:'100%'
+            width: '100%'
         }, 400);
         $(icon).animate({
             right: '10px'
@@ -15,15 +15,51 @@ $(function () {
 
     //blur event handler
     $(searchField).on('blur', function () {
-     if(searchField.val() == ''){
-        $(searchField).animate({
-            width:'45%'
-        }, 400, function(){});
-        $(icon).animate({
-            right:'360px'
-        }, 400, function(){});
-     }
+        if (searchField.val() == '') {
+            $(searchField).animate({
+                width: '45%'
+            }, 400, function () { });
+            $(icon).animate({
+                right: '360px'
+            }, 400, function () { });
+        }
     });
 
-
+    $('#search-form').submit(function (e) {
+        e.preventDefault();
+    });
 })
+
+//search function
+function search() {
+    //clear results
+    $('#results').html('');
+    $('#buttons').html('');
+
+    //get input
+    q = $('#query').val();
+
+    //call API 
+    $.get(
+        "https://www.googleapis.com/youtube/v3/search", {
+        part: 'snippet, id',
+        q: q,
+        type: 'video',
+        key: 'AIzaSyBZg-PVM63bSB6db3WQGx97gVbHswfZSeo'
+    },
+        function (data) {
+            var nextPageToken = data.nextPageToken;
+            var prevPageToken = data.prevPageToken;
+
+            //console.log(data);
+
+            $.each(data.items, function(i, item){
+                //get output from response
+                var output = getOutput(item);
+
+                //display results
+                $('#results').append(output);
+            });
+        }
+    );
+}
