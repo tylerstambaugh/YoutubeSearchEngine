79501794c7431c3b612257f1080b34d10bfa8a66
Tyler Stambaugh <11870675+tylerstambaugh@users.noreply.github.com>
2022-10-31 19:56:47 -0400

    removed old codes

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-jQuery.browser = {};
-(function () {
-    jQuery.browser.msie = false;
-    jQuery.browser.version = 0;
-    if (navigator.userAgent.match(/MSIE ([0-9]+)\./)) {
-        jQuery.browser.msie = true;
-        jQuery.browser.version = RegExp.$1;
-    }
-})();
-
 // searchbar handlers
 $(function () {
     var searchField = $('#query');
